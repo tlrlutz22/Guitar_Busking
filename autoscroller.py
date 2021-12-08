2be2a027f192131959df7ee9073830f2292fe876
tlrlutz22 <68932247+tlrlutz22@users.noreply.github.com>
2021-12-07 19:01:46 -0600

    Create autoscroller.py

---
+++ autoscroller.py
@@ -0,0 +1,10 @@
+import pyautogui
+import re
+import time
+
+# while 0 < 1:
+pyautogui.click(1430, 136, button='left')
+pyautogui.dragTo(1428, 137, button='left')
+pyautogui.drag(0, 125, 3, button='left')
+time.sleep(3)
+pyautogui.click(1430, 136, button='left')
